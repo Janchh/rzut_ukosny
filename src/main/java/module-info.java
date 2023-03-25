3e2d54ce1d570007b6ca83a724b9d1aaa9f04697
Jan Chruśliński <01169737@pw.edu.pl>
2023-03-25 12:59:47 +0100

    pierwsze okno

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.example.rzut_ukosny {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.rzut_ukosny to javafx.fxml;
     exports com.example.rzut_ukosny;
 }
