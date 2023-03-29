27f70de05212ec91853b446fe8ef31d8b9480209
Jan Chruśliński <01169737@pw.edu.pl>
2023-03-29 10:54:49 +0200

    okno główne

--- src/main/java/com/example/rzut_ukosny/MainFrame.java
+++ src/main/java/com/example/rzut_ukosny/MainFrame.java
@@ -7,12 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
+public class MainFrame extends Application {
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFrame.class.getResource("main-frame.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Rzut ukośny");
         stage.setScene(scene);
         stage.show();
     }
